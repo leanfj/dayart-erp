727d6f8a161f43a9b0f77dce6ee1b51a39cdf0fd
Leandro Ferreira <leanfj@hotmail.com>
2023-02-27 00:10:02 -0300

    Colorir logs express

--- src/infrastructure/http/middlewares/logger.middleware.ts
+++ src/infrastructure/http/middlewares/logger.middleware.ts
@@ -1,10 +1,11 @@
 import { NextFunction, Request, Response } from "express";
+import colors from "colors/safe";
 
 export function loggerMiddleware(
   request: Request,
   response: Response,
   next: NextFunction
 ) {
-  console.log(`${request.method} ${request.path}`);
+  console.log(`${colors.bold(colors.green(request.method))} ${request.path}`);
   next();
 }

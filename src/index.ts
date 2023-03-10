a481278cf15f5b325ce9ef600c28f86a3d34691e
Leandro Ferreira <leanfj@hotmail.com>
2023-03-09 22:08:57 -0300

    Atualização de desenvolvimento

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-require('dotenv').config();
+require('dotenv').config(
+    {
+        path: process.env.NODE_ENV === 'development' ? '.env.development' : '.env'
+    }
+);
 
 import './infrastructure/http/server'
